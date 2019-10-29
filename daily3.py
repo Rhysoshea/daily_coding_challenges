43c3f61d4a6182456d28ffe0a5972184fd6ae99b
rhys <rhys.shea.10@ucl.ac.uk>
2019-10-29 17:08:05 +0900

    updated daily3

--- daily3.py
+++ daily3.py
@@ -20,10 +20,23 @@ class Node:
         self.left = left
         self.right = right
 
-    def serialize():
+def serialize(root):
+    if root is None:
+        return '#'
+    return '{} {} {}'.format(root.val, serialize(root.left), serialize(root.right))
 
-    def deserialize():
-
+def deserialize(data):
+    def helper():
+        val = next(vals)
+        if val == "#":
+            return None
+        node = Node(val)
+        node.left = helper()
+        node.right = helper()
+        return node
+    vals = iter(data.split())
+    return helper()
 
 node = Node('root', Node('left', Node('left.left')), Node('right'))
+# print (deserialize('root left left.left # # # right # #'))
 assert deserialize(serialize(node)).left.left.val == 'left.left'
