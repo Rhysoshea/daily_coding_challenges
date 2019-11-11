4ee1ec5791a69468b0f10f648f36ef893d639ef8
rhys <rhys.shea.10@ucl.ac.uk>
2019-11-11 18:18:15 +0900

    job scheduler

---
+++ daily10.py
@@ -0,0 +1,43 @@
+'''
+Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
+
+'''
+
+import time
+import threading
+
+'''
+Uses one thread per delay which can explode
+'''
+class Scheduler:
+    def __init__(self):
+        pass
+
+    def delay(self, f, n):
+        def sleep_then_call(n):
+            sleep(n / 1000)
+            f()
+        t = threading.Thread(target=sleep_then_call)
+        t.start()
+
+'''
+Uses one dedicated thread and stores the functions in a list with their designated timestamp to run by 
+'''
+
+class Scheduler:
+    def __init__(self):
+        self.fns = [] # tuple of (fn, time)
+        t = threading.Thread(target=self.poll)
+        t.start()
+
+    def poll(self):
+        while True:
+            now = time() * 1000
+            for fn, due in self.fns:
+                if now > due:
+                    fn()
+            self.fns = [(fn, due) for (fn, due) in self.fns if due > now]
+            sleep(0.01)
+
+    def delay(self, f, n):
+        self.fns.append((f, time() * 1000 + n))
