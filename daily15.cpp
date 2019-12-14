d4afc968192abf840215472a812da2d183e1bc7e
rhys <rhys.shea.10@ucl.ac.uk>
2019-12-14 18:46:24 +0900

    added bias test

--- daily15.cpp
+++ daily15.cpp
@@ -21,11 +21,22 @@ int random_generator(){
 }
 
 int main() {
-  int n = 100;
-  vector<int> stream;
-  for (size_t i=0; i<n; ++i){
-    stream.push_back(random_generator());
+  int n = 10000; //number of iterations to run code
+  vector<int> count(10,0); //numbers range from 1 to 10
+
+  // outer loop is a check to see if random element has any bias
+  for (int i=0; i<n; ++i) {
+    vector<int> stream;
+    for (size_t i=0; i<n; ++i){
+      stream.push_back(random_generator());
+    }
+    int el = random_element(stream);
+    count[el-1] += 1;
+    // cout << "The random element is " << el << endl;
   }
-  int el = random_element(stream);
-  cout << "The random element is " << el << endl;
+
+  for (int i=0; i<count.size(); ++i){
+    cout << "Number: " << i+1 << " count: " << count[i] << endl;
+  }
+
 }
