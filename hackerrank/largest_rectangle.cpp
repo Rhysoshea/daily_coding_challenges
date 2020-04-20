392ef6e63869c5535e16356275d42184d725ce51
rhys <rhys.shea.10@ucl.ac.uk>
2020-04-20 18:06:22 +0900

    largest rectangle

---
+++ hackerrank/largest_rectangle.cpp
@@ -0,0 +1,100 @@
+// https://www.hackerrank.com/challenges/largest-rectangle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues&h_r=next-challenge&h_v=zen
+
+// given an array of 'building heights', return the maximum area that a consecutive row of buildings will populate
+
+// example
+
+// [1,2,3,4,5]
+//                 5----
+//             4----    
+//         3---- -------
+//     2----|########### 
+// 1----    |###########
+// ---------------------
+// the biggest rectangle is the 3x3 so the answer is 9
+
+
+
+// #include <bits/stdc++.h>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the largestRectangle function below.
+long largestRectangle(vector<int> h) {
+
+    stack<int> mystack;
+
+    for (int i=0; i<h.size(); i++) {
+        if (i+1 >= h.size()){
+
+        }
+        else if (h[i] <= h[i+1]) {
+            mystack.push(i);
+        }
+    }
+
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split_string(h_temp_temp);
+
+    vector<int> h(n);
+
+    for (int i = 0; i < n; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    long result = largestRectangle(h);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
