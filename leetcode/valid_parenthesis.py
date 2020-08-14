50bcb774054a1d409a43ca0096a033b4d268d8d5
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-08-14 18:48:23 +0900

    bracket check

---
+++ leetcode/valid_parenthesis.py
@@ -0,0 +1,48 @@
+"""
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+"""
+
+class Solution:
+
+    def init(self):
+        pass
+
+    def isValid( s):
+
+        hash = {
+            "[":"]",
+            "(":")",
+            "{":"}"
+        }
+
+        stack = []
+
+        for i in range(len(s)):
+            if s[i] in ["[", "(", "{"]:
+                stack.append(hash[s[i]])
+            else:
+                if not stack or s[i] != stack.pop():
+                    return False 
+
+        if stack:
+            return False 
+
+        return True
+
+solve = Solution
+
+assert(solve.isValid("()") == True)
+assert(solve.isValid("()[]{}") == True)
+assert(solve.isValid("(]") == False)
+assert(solve.isValid("([)]") == False)
+assert(solve.isValid("{[]}") == True)
+assert(solve.isValid("") == True)
+assert(solve.isValid("[") == False)
+assert(solve.isValid("]") == False)
